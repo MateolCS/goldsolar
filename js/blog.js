c32cf446c7aec9ced09ff0b3e2f992d386f5d4aa
MateoLCS <mateusz.namysl0318@gmail.com>
2022-04-04 23:55:28 +0200

    Created basic post display logic and page handling

--- js/blog.js
+++ js/blog.js
@@ -1,16 +1,20 @@
-const getContent = () => {
-    fetch('../data/blogposts.json').then(async response => {
-        try {
-         const data = await response.json()
-       } catch(error) {
-         console.log('Error happened here!')
-         console.error(error)
-       }
-      })
-}
+const nextPage = document.querySelector('#main__content__nextpage')
+const prevPage = document.querySelector('#main__content__prevpage')
+const firstPage = document.querySelector('#main__content__page1')
+const secondPage = document.querySelector('#main__content__page2')
+const thirdPage = document.querySelector('#main__content__page3')
+const fourthPage = document.querySelector('#main__content__page4')
+let currentPage = 0
 
-const test = require('../data/blogposts.json')
-console.log(test)
+const getBlogPosts = async() => {
+    const res = await fetch('../data/blogposts.json')
+    const data = await res.json()
+
+    displayBlogPosts(data[currentPage])
+    handlePrevPage()
+    handleNexPage()
+
+}
 
 const createBlogPost = (title, description, link, img) => {
     const blogPost = document.createElement('div')
@@ -38,7 +42,44 @@ const createBlogPost = (title, description, link, img) => {
 
 const displayBlogPosts = (blogPosts) => {
     const container = document.querySelector('.main__content__grid')
+    container.innerHTML = ''
     blogPosts.forEach(blogPost => {
         container.appendChild(createBlogPost(blogPost.title, blogPost.description, blogPost.link, blogPost.img))
     })
 }
+
+const handlePrevPage = () => {
+    if (currentPage === 0) {
+        prevPage.style.display = 'none'
+    } else {
+        prevPage.style.display = 'block'
+    }
+    
+}
+
+const handleNexPage = () => {
+    if (currentPage === 3) {
+        nextPage.style.display = 'none'
+    } else {
+        nextPage.style.display = 'block'
+    }
+    
+}
+
+document.onload = getBlogPosts()
+
+nextPage.addEventListener('click', () => {
+    currentPage++
+    getBlogPosts()
+})
+
+prevPage.addEventListener('click', () => {
+    currentPage--
+    getBlogPosts()
+})
+
+secondPage.addEventListener('click', () => {
+    currentPage = 1
+    getBlogPosts()
+})
+
